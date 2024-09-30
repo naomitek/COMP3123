2c527c5c38fffa7f86d1e61b91889168b46fb25b
naomitek <156701780+naomitek@users.noreply.github.com>
2024-09-29 17:08:22 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// GET /hello
+app.get('/hello', (req, res) => {
+    res.send('Hello Express JS');
+});
+
+// GET /user
+app.get('/user', (req, res) => {
+    const firstname = req.query.firstname || 'ali ';
+    const lastname = req.query.lastname || 'nana';
+    res.json({ firstname, lastname });
+});
+
+// POST /user
+app.post('/user/:firstname/:lastname', (req, res) => {
+    const { firstname, lastname } = req.params;  
+    res.json({ firstname, lastname });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
+
